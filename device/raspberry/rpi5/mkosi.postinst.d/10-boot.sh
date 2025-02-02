05956b4af23f83f1fcee87f3e2ac40700d50d2a0
alexander stefanov <alexander@mezon.ru>
2025-02-02 15:08:01 +0300

    add rpi5 stuff

--- device/raspberry/rpi5/mkosi.postinst.d/10-boot.sh
+++ device/raspberry/rpi5/mkosi.postinst.d/10-boot.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+set -x
+set -e
+BOOTDIR=${BUILDROOT}/boot/firmware
+ls -la ${BOOTDIR}
+mkdir -p ${BOOTDIR}
 # Find vmlinuz and initrd files in the /boot directory
 vmlinuz_file=$(find ${BUILDROOT}/boot -name 'vmlinuz-*' -type f | head -n 1)
 initrd_file=$(find ${BUILDROOT}/boot -name 'initrd-*.img' -type f | head -n 1)
@@ -13,10 +18,15 @@ fi
 kernel_name=$(basename "$vmlinuz_file")
 initrd_name=$(basename "$initrd_file")
 
-# Append the required lines to /boot/config.txt
+printf "move kernel, initrd and dtb files to /boot/firmware/"
+mv ${vmlinuz_file} ${BOOTDIR}
+mv ${initrd_file} ${BOOTDIR}
+mv ${BUILDROOT}/boot/dtb-*/broadcom/bcm*.dtb ${BOOTDIR}
+
+# Append the required lines to /boot/firmware/config.txt#
 {
     printf "kernel=%s\n" "$kernel_name"
     printf "initramfs %s followkernel\n" "$initrd_name"
-} >> ${BUILDROOT}/boot/config.txt
+  } >> ${BOOTDIR}/config.txt
 
-printf "The kernel and initrd entries have been added to /boot/config.txt.\n"
+printf "The kernel and initrd entries have been added to /boot/firmware/config.txt.\n"
