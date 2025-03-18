9c2323972770d88013c61fb1f19356cab323cd5c
alexander stefanov <alexander@mezon.ru>
2025-03-18 11:04:02 +0300

    add pretty warning for case when spl not available

--- device/allwinner/repkapi4/mkosi.postoutput.d/10-write-uboot.sh
+++ device/allwinner/repkapi4/mkosi.postoutput.d/10-write-uboot.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 # SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
-set -x
-UBOOT_DIR="${SRCDIR}/mkosi"
+UBOOT_SPL="${SRCDIR}/u-boot-sunxi-with-spl.bin"
 
-env
-pushd ${OUTPUTDIR}
-printf "write bootloader to the image...\n"
-ls -la ../src/
-#dd if=${UBOOT_DIR}/idbloader.img of=image.raw seek=64 conv=notrunc
-#dd if=${UBOOT_DIR}/u-boot.itb of=image.raw seek=16384 conv=notrunc
+# Check if U-Boot SPL file exists
+if [ ! -f "$UBOOT_SPL" ]; then
+    printf "\n"
+    printf "+-----------------------------+\n"
+    printf "|       _                 _   |\n"
+    printf "| _   _| |__   ___   ___ | |_ |\n"
+    printf "|| | | | '_ \ / _ \ / _ \| __||\n"
+    printf "|| |_| | |_) | (_) | (_) | |_ |\n"
+    printf "| \__,_|_.__/ \___/ \___/ \__||\n"
+    printf "+-----------------------------+\n"
+    printf "\n"
+    printf "  ⚠️  U-BOOT file u-boot-sunxi-with-spl.bin is missing!  ⚠️\n"
+    printf "  Please compile U-Boot before running this script.\n"
+    exit 0
+fi
+
+pushd "${OUTPUTDIR}"
+printf "Writing bootloader to the image...\n"
+dd if="${UBOOT_SPL}" of=image.raw bs=1k seek=128 conv=notrunc
 popd
